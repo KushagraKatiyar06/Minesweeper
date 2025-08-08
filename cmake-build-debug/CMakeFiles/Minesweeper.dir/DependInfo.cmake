21b694b7fddb73d2dad8899a50c69f8b8d84e804
KushagraKatiyar06 <playkushagra@gmail.com>
2025-08-08 01:38:24 -0400

    debug files

---
+++ cmake-build-debug/CMakeFiles/Minesweeper.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kkati/Kushagra/Projects/Minesweeper/Board.cpp" "CMakeFiles/Minesweeper.dir/Board.cpp.obj" "gcc" "CMakeFiles/Minesweeper.dir/Board.cpp.obj.d"
+  "C:/Users/kkati/Kushagra/Projects/Minesweeper/Tile.cpp" "CMakeFiles/Minesweeper.dir/Tile.cpp.obj" "gcc" "CMakeFiles/Minesweeper.dir/Tile.cpp.obj.d"
+  "C:/Users/kkati/Kushagra/Projects/Minesweeper/main.cpp" "CMakeFiles/Minesweeper.dir/main.cpp.obj" "gcc" "CMakeFiles/Minesweeper.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Minesweeper.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Minesweeper.dir/Board.cpp.obj"
+  "CMakeFiles/Minesweeper.dir/Board.cpp.obj.d"
+  "CMakeFiles/Minesweeper.dir/Tile.cpp.obj"
+  "CMakeFiles/Minesweeper.dir/Tile.cpp.obj.d"
+  "CMakeFiles/Minesweeper.dir/main.cpp.obj"
+  "CMakeFiles/Minesweeper.dir/main.cpp.obj.d"
+  "Minesweeper.exe"
+  "Minesweeper.exe.manifest"
+  "Minesweeper.pdb"
+  "libMinesweeper.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Minesweeper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
